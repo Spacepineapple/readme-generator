6ecb5cef64db8411488e1528ab0e0ad5719f56df
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-16 22:20:39 +0000

    Add table of contents and description headings to readme structure in generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,16 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Table of Contents
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Contributing](#contributing)
+  5. [Testing](#testing)
+  6. [Contact](#contact)
+  7. [License](#license)
+  ## Description
+  ${data.description}
 `;
 }
 
