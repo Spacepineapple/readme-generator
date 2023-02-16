ee20fd66fda4f06762e81cd4b68999d3de8a85df
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-16 20:39:54 +0000

    Add all questions to inquirer prompt

--- index.mjs
+++ index.mjs
@@ -16,7 +16,7 @@ const questions = [
     "Please select a license"
 ];
 
-let [title, description, installation, usage, contributing, instructions, username, address, license] = questions;
+let [title, description, installation, usage, contributing, test, username, address, license] = questions;
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -24,20 +24,66 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 async function init() {
-    let {projectTitle, projectDescription} = await inquirer
+    let {projectTitle, projectDescription, projectInstallation, projectUsage, projectContributing, projectTest, projectUsername, projectAddress, projectLicense} = await inquirer
     .prompt([
         {
-            type: 'input',
-            name: 'projectTitle',
+            type: "input",
+            name: "projectTitle",
             message: title,
         },
         {
-            type: 'input',
-            name: 'projectDescription',
+            type: "input",
+            name: "projectDescription",
             message: description,
         },
+        {
+            type: "input",
+            name: "projectInstallation",
+            message: installation,
+        },
+        {
+            type: "input",
+            name: "projectUsage",
+            message: usage,
+        },
+        {
+            type: "input",
+            name: "projectContributing",
+            message: contributing,
+        },
+        {
+            type: "input",
+            name: "projectTest",
+            message: test,
+        },
+        {
+            type: 'input',
+            name: 'projectUsername',
+            message: username,
+        },
+        {
+            type: 'input',
+            name: 'projectAddress',
+            message: address,
+        },
+        {
+            type: 'list',
+            name: 'projectLicense',
+            message: license,
+            choices: [
+                "Creative Commons Zero v1.0 Universal",
+                "Creative Commons Attribution 4.0",
+                "Creative Commons Attribution Share Alike 4.0",
+                "Do What The F*ck You Want To Public License",
+                "European Union Public License 1.1",
+                "MIT",
+                "Mozilla Public License 2.0",
+                "Open Software License 3.0",
+                "The Unlicense"
+            ],
+        },
     ])
-    console.log(projectTitle, projectDescription);
+    console.log(projectTitle, projectDescription, projectInstallation, projectUsage, projectContributing, projectTest, projectUsername, projectAddress, projectLicense);
 }
 
 // function call to initialize program
