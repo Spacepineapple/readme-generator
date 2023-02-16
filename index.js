386f3265548a0d9250451da768b5af985aec5d38
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-16 20:09:14 +0000

    Add input prompt questions to questions array

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the title of your project?",
+    "Please give a short description of your project",
+    "Please provide any installation instructions for your project",
+    "Please provide usage information for your project",
+    "Please mention whether you are happy for others to contribute to your project",
+    "Please provide test instructions for your project",
+    "Please provide your GitHub username",
+    "Please provide your email address",
+    "Please select a license"
 ];
 
 // function to write README file
