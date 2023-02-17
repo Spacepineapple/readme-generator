a2ef6ace9b9766ba92d4130e2a640eeab25e3c52
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-17 18:35:08 +0000

    Add all headings and variables to markdown generated in generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,8 +9,21 @@ function generateMarkdown(data) {
   5. [Testing](#testing)
   6. [Contact](#contact)
   7. [License](#license)
-  ## Description
+  ## Description <a id="description"></a>
   ${data.description}
+  ## Installation <a id="installation"></a>
+  ${data.installation}
+  ## Usage <a id="usage"></a>
+  ${data.usage}
+  ## Contributing <a id="contributing"></a>
+  ${data.contributing}
+  ## Testing <a id="testing></a>
+  ${data.testing}
+  ## Contact <a id="contact"></a>
+  ${data.email}
+  ${data.username}
+  ## License <a id="license></a>
+  ${data.license}
 `;
 }
 
